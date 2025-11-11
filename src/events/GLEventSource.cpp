ea1c30c25a650b0ac5bfcf4c110549a10b559b99
Dalibor Trampota <dalibor.trampota@autodesk.com>
2025-11-11 09:05:53 +0100

    get rid of event system

--- src/events/GLEventSource.cpp
+++
@@ -1,8 +0,0 @@
-#include "LWGL/events/GLEventSource.h"
-#include "LWGL/events/GLEventSite.h"
-
-#include "LWGL/events/GLEvents.h"
-
-void GLEventSource::fireWindowResizeEvent(ResizeEvent* pEvent) {
-    engine::EventSource::fireEvent(&GLEventSite::windowResizeEvent, pEvent);
-};
