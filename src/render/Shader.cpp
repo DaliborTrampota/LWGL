fc69be2e430f9450b29c2aebd11d64e0df68d6f0
Dalibor Trampota <dalibor.trampota@autodesk.com>
2025-11-29 12:49:14 +0100

    shader better log

--- src/render/Shader.cpp
+++ src/render/Shader.cpp
@@ -24,7 +24,8 @@ Shader::Shader(const char* path, ShaderType type) {
 
     std::ifstream R(path);
     if (!R.is_open()) {
-        printf("ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ");
+        printf("ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: %s\n", path);
+        return;
     }
     const std::string content(
         (std::istreambuf_iterator<char>(R)), std::istreambuf_iterator<char>()
