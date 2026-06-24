73f250bd0afc81093fae26e6e863b40a188ff8d7
Dalibor Trampota <dalibor.trampota@gmail.com>
2026-06-24 11:25:54 +0200

    add cubemaparray type

---
+++ include/LWGL/texture/CubeMapArray.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "TextureBase.h"
+
+namespace gl {
+
+    class CubeMapArray : public TextureBase {
+      public:
+        CubeMapArray(bool immutable = true) : TextureBase(TextureType::CubeMapArray, immutable) {}
+
+        void create(TextureParams params);
+        void allocate(TextureStorage storage);
+        void upload(int layer, CubeFace face, int width, ImageFormat format, Data data);
+        // void upload(int layer, CubeFace face, const ImageData& imageData);
+        // void upload(int layer, CubeFace face, const RawImageData& rawImageData);
+
+        void bind(int layer);
+        void unbind(int layer);
+        void bindFace(int layer, CubeFace face);
+        void unbindFace(int layer, CubeFace face);
+
+
+        int width() { return m_width; }
+        int channels() { return m_channels; }
+
+        int layers() { return m_layers; }
+        int layerFaces() { return m_layers * 6; }
+
+      private:
+        int m_width = 0;
+        int m_layers = 0;
+        int m_channels = 0;
+    };
+}  // namespace gl
---
+++ src/texture/CubeMapArray.cpp
@@ -0,0 +1,89 @@
+#include "LWGL/texture/CubeMapArray.h"
+#include "../detail/TexturePrivate.h"
+
+
+#include <glad/glad.h>
+#include <stdexcept>
+
+
+using namespace gl;
+
+void CubeMapArray::create(TextureParams params) {
+    if (m_id != 0)
+        throw std::runtime_error("CubeMapArray already created");
+
+    if (m_immutable) {
+        glCreateTextures(GL_TEXTURE_CUBE_MAP_ARRAY, 1, &m_id);
+    } else {
+        glGenTextures(1, &m_id);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP_ARRAY, m_id);
+    }
+    detail::ConfigureTexture(m_id, params);
+}
+
+void CubeMapArray::allocate(TextureStorage storage) {
+    if (m_id == 0)
+        throw std::runtime_error("CubeMapArray not created");
+
+    m_width = storage.width;
+    m_layers = storage.depth;
+    // m_channels = storage.channels; // TODO deduce from format?
+
+    if (m_immutable) {
+        detail::Data3DImmutable(
+            m_id, m_width, m_width, layerFaces(), storage.format, storage.dataType
+        );
+    } else {
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP_ARRAY, m_id);
+        detail::Data3D(
+            GL_TEXTURE_CUBE_MAP_ARRAY,
+            m_width,
+            m_width,
+            layerFaces(),
+            storage.format,
+            nullptr,
+            storage.dataType
+        );
+    }
+}
+
+void CubeMapArray::upload(int layer, CubeFace face, int width, ImageFormat format, Data data) {
+    if (m_id == 0)
+        throw std::runtime_error("CubeMapArray not created");
+
+    if (width <= 0)
+        throw std::runtime_error("Width must be greater than zero");
+
+    if (m_immutable) {
+        detail::SubData3D(
+            m_id, 0, 0, layer * 6 + static_cast<int>(face), width, width, 1, format, data
+        );
+    } else {
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP_ARRAY, m_id);
+        detail::SubData3D(
+            GL_TEXTURE_CUBE_MAP_ARRAY,
+            0,
+            0,
+            layer * 6 + static_cast<int>(face),
+            width,
+            width,
+            1,
+            format,
+            data
+        );
+    }
+}
+
+
+void CubeMapArray::bind(int layer) {
+    // glBindTexture(detail::toGLTexture(m_type), m_id);
+}
+
+void CubeMapArray::unbind(int layer) {}
+
+void CubeMapArray::bindFace(int layer, CubeFace face) {}
+
+void CubeMapArray::unbindFace(int layer, CubeFace face) {}
